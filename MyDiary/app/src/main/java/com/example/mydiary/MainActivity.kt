af6dcb0a77cef171aa2f3ccbe5399cbe6512fa91
bljboy <bljboy55@gmail.com>
2020-03-18 20:56:26 +0800

    Update read-write access permission,Permission popup window

--- MyDiary/app/src/main/java/com/example/mydiary/MainActivity.kt
+++ MyDiary/app/src/main/java/com/example/mydiary/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.example.mydiary
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
-
+    //先定义
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf(
+        "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE"
+    )
 
     @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        verifyStoragePermissions(this)
+
+
         var shared = getSharedPreferences("Info", Context.MODE_PRIVATE);
         var editor = shared.edit()
         editor.putString("account", "123")
@@ -76,5 +88,25 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //然后通过一个函数来申请
+    fun verifyStoragePermissions(activity: Activity?) {
+        try {
+            //检测是否有写的权限
+            val permission = ActivityCompat.checkSelfPermission(
+                activity!!,
+                "android.permission.WRITE_EXTERNAL_STORAGE"
+            )
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
 }
